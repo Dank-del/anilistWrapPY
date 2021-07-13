60e3a0a0d11d7fe8cf6deb79505081e1ee035cb1
Sayan Biswas <sayan@pokurt.me>
2021-07-13 18:02:28 +0530

    use error class

--- anilistWrapPY/Character/request.py
+++ anilistWrapPY/Character/request.py
@@ -1,6 +1,7 @@
 # The contents of this file is free and unencumbered software released into the
 # public domain. For more information, please refer to <http://unlicense.org/>
-
+from anilistWrapPY.errors.types import AniListError
+from anilistWrapPY.errors.ex_class import AniListException
 from anilistWrapPY.Character.types import AniListCharacter
 from anilistWrapPY.Character.graphql import character_query
 import httpx
@@ -9,4 +10,7 @@ import httpx
 def GetCharacter(search: str, baseUrl: str) -> AniListCharacter:
     variables = {"search": search}
     r = (httpx.post(baseUrl, json={"query": character_query, "variables": variables})).json()
-    return AniListCharacter(**r)
+    try:
+        return AniListCharacter(**r)
+    except AniListException:
+        return AniListError(**r)
--- anilistWrapPY/Media/request.py
+++ anilistWrapPY/Media/request.py
@@ -1,6 +1,7 @@
 # The contents of this file is free and unencumbered software released into the
 # public domain. For more information, please refer to <http://unlicense.org/>
-
+from anilistWrapPY.errors.types import AniListError
+from anilistWrapPY.errors.ex_class import AniListException
 from anilistWrapPY.Media.types import AniListMedia
 from anilistWrapPY.Media.graphql import media_query
 import httpx
@@ -9,4 +10,7 @@ import httpx
 def GetMedia(search: str, baseUrl: str) -> AniListMedia:
     variables = {"search": search}
     r = (httpx.post(baseUrl, json={"query": media_query, "variables": variables})).json()
-    return AniListMedia(**r)
+    try:
+        return AniListMedia(**r)
+    except AniListException:
+        return AniListError(**r)
